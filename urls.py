ed8f1631bcfe0eece2df49413024206116526f85
Naresh162001 <88872578+Naresh162001@users.noreply.github.com>
2021-12-11 15:15:42 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('sample',include('sample.urls')),
     path('',include('banking_System.urls')),
     path('admin/', admin.site.urls),
 ]
